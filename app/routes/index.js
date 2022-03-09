c7302fc9d840e685927db2e2a184d72e357ff597
santy <santy2691@hotmail.com>
2022-03-09 23:24:01 +0100

    controller

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const User = require('../User')
+const User = require('../controlles/User')
 
 router.get('/', (req,res)=>{
     res.sendFile('index.html');
